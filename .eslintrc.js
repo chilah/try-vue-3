2d6a839e63e8e53d05b2581dc79add2a088f1ac7
Jirapong Kanha <pasitaless@gmail.com>
2022-02-12 21:16:29 +0700

    update ts version and eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,12 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    "vue/setup-compiler-macros": true,
   },
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
+    "plugin:vue/vue3-recommended",
     "@vue/typescript/recommended",
     "@vue/prettier",
     "@vue/prettier/@typescript-eslint",
